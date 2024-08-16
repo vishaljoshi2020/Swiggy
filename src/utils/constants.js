a355f59ddd43c3cede1767cf2d7032a6513d2539
vishal joshigit config --global user.name vishal <vishaljoshi8590@gmail.com>
2024-08-16 18:36:02 +0530

    after the menu wiout css

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 export const LOGO_URL =
   "https://cdn.vectorstock.com/i/1000v/01/03/creative-letter-vj-logo-typography-vector-30830103.jpg";
+
+export const MENU_URL =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9698196&lng=77.7499721&restaurantId="; 
